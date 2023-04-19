89d2f92e0af61b80d8d4de852e43bc1bc8671fde
kashiash <kashiash@gmail.com>
2023-04-19 10:35:25 +0200

    Initial Commit

---
+++ Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Jacek Placek on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
