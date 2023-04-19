725c025a47e6e1eee18c94ff40de6026bea3f0fa
kashiash <kashiash@gmail.com>
2023-04-19 14:42:52 +0200

    moja pierwsza wersja

--- Test1/ContentView.swift
+++ Test1/ContentView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var koszt = 0.0
+    @State private var ludzie = 2
+    @State private var tip = 20
+    
+    
+    let tips = [5,10,15,20,25,30,0]
+    
+    var total: Double {
+        let lud = Double(ludzie + 2)
+        let tipp = Double(tip)
+        
+        let tipV = koszt / 100 * tipp
+        let Gtotal = koszt + tipV
+        let NaOsobe = Gtotal / lud
+        
+        return NaOsobe
     }
-}
+    
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Należność", value: $koszt, format: .currency(code: Locale.current.currency?.identifier ?? "PLN"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("ilość osób ", selection: $ludzie) {
+                        ForEach(2..<100) {
+                            Text("\($0) osoby")
+                        }
+                    }
+                }
+                
+                
+                Section {
+                    Picker("Ile procent napiwku", selection: $tip) {
+                        ForEach(tips, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Jak dużo napiwku chcesz zostawić")
+                }
+                Section {
+                    Text(total, format: .currency(code:  Locale.current.currency?.identifier ?? "PLN"))
+                }
+                
+                
+                
+            .navigationTitle("Kalkulator dla ułomnych")
+            .navigationBarTitleDisplayMode(.inline)
+            }
+         }
+     }
+ }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Test1/Test2.swift
@@ -0,0 +1,20 @@
+//
+//  Test2.swift
+//  Test1
+//
+//  Created by Jacek Placek on 19/04/2023.
+//
+
+import SwiftUI
+
+struct Test2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Test2_Previews: PreviewProvider {
+    static var previews: some View {
+        Test2()
+    }
+}
